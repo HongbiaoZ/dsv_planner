576c84f85318086e914eaad06188cb21fd87514a
biao <biao>
2021-02-02 20:51:27 -0500

    format changes

--- src/dsvplanner/graph_planner/src/graph_planner.cpp
+++ src/dsvplanner/graph_planner/src/graph_planner.cpp
@@ -18,53 +18,45 @@ Modified and maintained by Hongbiao Zhu (hongbiaz@andrew.cmu.edu)
 
 #include <string>
 
-namespace graph_planner_ns
-{
-bool GraphPlanner::readParameters()
-{
-  if (!nh_.getParam("world_frame_id", world_frame_id_))
-  {
+namespace graph_planner_ns {
+bool GraphPlanner::readParameters() {
+  if (!nh_.getParam("world_frame_id", world_frame_id_)) {
     ROS_ERROR("Cannot read parameter: world_frame_id_");
     return false;
   }
-  if (!nh_.getParam("graph_planner_command_topic", graph_planner_command_topic_))
-  {
+  if (!nh_.getParam("graph_planner_command_topic",
+                    graph_planner_command_topic_)) {
     ROS_ERROR("Cannot read parameter: graph_planner_command_topic_");
     return false;
   }
-  if (!nh_.getParam("graph_planner_status_topic", graph_planner_status_topic_))
-  {
+  if (!nh_.getParam("graph_planner_status_topic",
+                    graph_planner_status_topic_)) {
     ROS_ERROR("Cannot read parameter: graph_planner_status_topic_");
     return false;
   }
-  if (!nh_.getParam("pub_waypoint_topic", pub_waypoint_topic_))
-  {
+  if (!nh_.getParam("pub_waypoint_topic", pub_waypoint_topic_)) {
     ROS_ERROR("Cannot read parameter: pub_waypoint_topic_");
     return false;
   }
-  if (!nh_.getParam("pub_path_topic", pub_path_topic_))
-  {
+  if (!nh_.getParam("pub_path_topic", pub_path_topic_)) {
     ROS_ERROR("Cannot read parameter: pub_path_topic_");
     return false;
   }
-  if (!nh_.getParam("sub_odometry_topic", sub_odometry_topic_))
-  {
+  if (!nh_.getParam("sub_odometry_topic", sub_odometry_topic_)) {
     ROS_ERROR("Cannot read parameter: sub_odometry_topic_");
     return false;
   }
-  if (!nh_.getParam("sub_graph_topic", sub_graph_topic_))
-  {
+  if (!nh_.getParam("sub_graph_topic", sub_graph_topic_)) {
     ROS_ERROR("Cannot read parameter: sub_graph_topic_");
     return false;
   }
 
-  if (!nh_.getParam("kLookAheadDist", kLookAheadDist))
-  {
+  if (!nh_.getParam("kLookAheadDist", kLookAheadDist)) {
     ROS_ERROR("Cannot read parameter: kLookAheadDist");
     return false;
   }
-  if (!nh_.getParam("kWaypointProjectionDistance", kWaypointProjectionDistance))
-  {
+  if (!nh_.getParam("kWaypointProjectionDistance",
+                    kWaypointProjectionDistance)) {
     ROS_ERROR("Cannot read parameter: kWaypointProjectionDistance");
     return false;
   }
@@ -72,100 +64,90 @@ bool GraphPlanner::readParameters()
   return true;
 }
 
-void GraphPlanner::publishPath()
-{
+void GraphPlanner::publishPath() {
   nav_msgs::Path planned_path;
   planned_path.header.frame_id = world_frame_id_;
   planned_path.header.stamp = ros::Time::now();
 
-  if (in_progress_ && !planned_path_.empty())
-  {
+  if (in_progress_ && !planned_path_.empty()) {
     planned_path.poses.resize(planned_path_.size() + 1);
     planned_path.poses[0].pose.position = robot_pos_;
-    for (int i = 0; i < planned_path_.size(); i++)
-    {
+    for (int i = 0; i < planned_path_.size(); i++) {
       planned_path.poses[i + 1].pose.position = planned_path_[i];
     }
-  }
-  else
-  {
+  } else {
     planned_path.poses.resize(1);
     planned_path.poses[0].pose.position = robot_pos_;
   }
   graph_planner_path_pub_.publish(planned_path);
 }
 
-void GraphPlanner::odometryCallback(const nav_msgs::Odometry::ConstPtr& odometry_msg)
-{
+void GraphPlanner::odometryCallback(
+    const nav_msgs::Odometry::ConstPtr &odometry_msg) {
   double roll, pitch, yaw;
   geometry_msgs::Quaternion geo_quat = odometry_msg->pose.pose.orientation;
-  tf::Matrix3x3(tf::Quaternion(geo_quat.x, geo_quat.y, geo_quat.z, geo_quat.w)).getRPY(roll, pitch, yaw);
+  tf::Matrix3x3(tf::Quaternion(geo_quat.x, geo_quat.y, geo_quat.z, geo_quat.w))
+      .getRPY(roll, pitch, yaw);
 
   robot_yaw_ = yaw;
   robot_pos_ = odometry_msg->pose.pose.position;
 }
 
-void GraphPlanner::commandCallback(const graph_planner::GraphPlannerCommand::ConstPtr& msg)
-{
+void GraphPlanner::commandCallback(
+    const graph_planner::GraphPlannerCommand::ConstPtr &msg) {
   graph_planner_command_ = *msg;
-  if (graph_planner_command_.command == graph_planner::GraphPlannerCommand::COMMAND_GO_TO_LOCATION)
-  {
+  if (graph_planner_command_.command ==
+      graph_planner::GraphPlannerCommand::COMMAND_GO_TO_LOCATION) {
     previous_shortest_path_size_ = 100000;
     wrong_id_shortest_path_size_ = 100000;
     previous_shortest_path_size_when_pathrewind = 100000;
   }
 }
 
-void GraphPlanner::graphCallback(const graph_utils::TopologicalGraph::ConstPtr& graph_msg)
-{
+void GraphPlanner::graphCallback(
+    const graph_utils::TopologicalGraph::ConstPtr &graph_msg) {
   planned_graph_ = *graph_msg;
 }
 
-void GraphPlanner::publishInProgress(bool in_progress)
-{
-  in_progress_ = in_progress;  // used mainly for debugging
+void GraphPlanner::publishInProgress(bool in_progress) {
+  in_progress_ = in_progress; // used mainly for debugging
 
   graph_planner::GraphPlannerStatus status;
-  if (in_progress)
-  {
+  if (in_progress) {
     status.status = graph_planner::GraphPlannerStatus::STATUS_IN_PROGRESS;
-  }
-  else
-  {
+  } else {
     status.status = graph_planner::GraphPlannerStatus::STATUS_DISABLED;
-    state_ = ALL_OTHER_STATES;  // reset
+    state_ = ALL_OTHER_STATES; // reset
   }
 
   graph_planner_status_pub_.publish(status);
 }
 
-void GraphPlanner::alterAndPublishWaypoint()
-{
+void GraphPlanner::alterAndPublishWaypoint() {
   // Ship it!
-  waypoint_.header.frame_id = world_frame_id_;  // added by Hong in 20191203
-  waypoint_.header.stamp = ros::Time::now();    // added by Hong in 20191203
+  waypoint_.header.frame_id = world_frame_id_; // added by Hong in 20191203
+  waypoint_.header.stamp = ros::Time::now();   // added by Hong in 20191203
   waypoint_pub_.publish(waypoint_);
 }
 
-bool GraphPlanner::goToVertex(int current_vertex_idx, int goal_vertex_idx)
-{
+bool GraphPlanner::goToVertex(int current_vertex_idx, int goal_vertex_idx) {
   std::vector<int> shortest_path;
-  graph_utils_ns::ShortestPathBtwVertex(shortest_path, planned_graph_, current_vertex_idx, goal_vertex_idx);
+  graph_utils_ns::ShortestPathBtwVertex(shortest_path, planned_graph_,
+                                        current_vertex_idx, goal_vertex_idx);
 
-  if (shortest_path.size() <= 0)
-  {
+  if (shortest_path.size() <= 0) {
     // ROS_WARN("Graph planner did not find path to selected goal!");
 
     // clear the saved path
     planned_path_.clear();
 
     return false;
-  }
-  else if (shortest_path.size() <= 2)
-  {
-    // picked my current vertex -- therefore pick currently visible and unvisited opening
+  } else if (shortest_path.size() <= 2) {
+    // picked my current vertex -- therefore pick currently visible and
+    // unvisited opening
     geometry_msgs::Point next_waypoint =
-        planned_graph_.vertices[shortest_path.back()].location;  // why vertices[0] but not vertices[goal_vertex_idx]???
+        planned_graph_.vertices[shortest_path.back()]
+            .location; // why vertices[0] but not vertices[goal_vertex_idx]???
 
     waypoint_.header.stamp = ros::Time::now();
     waypoint_.point.x = next_waypoint.x;
@@ -179,71 +161,59 @@ bool GraphPlanner::goToVertex(int current_vertex_idx, int goal_vertex_idx)
 
     alterAndPublishWaypoint();
     return false;
-  }
-  else
-  {
+  } else {
     int next_vertex_id;
     next_vertex_id = shortest_path[0];
-    next_vertex_id =
-        graph_utils_ns::GetFirstVertexBeyondThreshold(robot_pos_, shortest_path, planned_graph_, kLookAheadDist);
+    next_vertex_id = graph_utils_ns::GetFirstVertexBeyondThreshold(
+        robot_pos_, shortest_path, planned_graph_, kLookAheadDist);
     std::vector<int> next_shortest_path;
-    graph_utils_ns::ShortestPathBtwVertex(next_shortest_path, planned_graph_, next_vertex_id, goal_vertex_idx);
+    graph_utils_ns::ShortestPathBtwVertex(next_shortest_path, planned_graph_,
+                                          next_vertex_id, goal_vertex_idx);
 
-    // when the path is bypassed a thin wall, follow the path one vertex by one vertex.
-    bool pathRewind = graph_utils_ns::PathCircleDetect(shortest_path, planned_graph_, next_vertex_id);
+    // when the path is bypassed a thin wall, follow the path one vertex by one
+    // vertex.
+    bool pathRewind = graph_utils_ns::PathCircleDetect(
+        shortest_path, planned_graph_, next_vertex_id);
     // std::cout << "pathrewind = " << pathRewind << std::endl;
-    if (pathRewind)
-    {
+    if (pathRewind) {
       wrong_id_ = true;
       wrong_id_shortest_path_size_ = next_shortest_path.size();
     }
 
-    if (!wrong_id_)
-    {
+    if (!wrong_id_) {
       // prevent back and forth between two vertices
-      if (next_shortest_path.size() > previous_shortest_path_size_)
-      {
+      if (next_shortest_path.size() > previous_shortest_path_size_) {
         next_vertex_id = previous_vertex_id_;
         backTraceCount_++;
-        if (backTraceCount_ > 15)
-        {
+        if (backTraceCount_ > 15) {
           backTraceCount_ = 0;
           previous_shortest_path_size_ = 100000;
         }
-      }
-      else
-      {
+      } else {
         previous_vertex_id_ = next_vertex_id;
         previous_shortest_path_size_ = next_shortest_path.size();
       }
-    }
-    else
-    {
+    } else {
       next_vertex_id = shortest_path[1];
-      if (shortest_path.size() >= previous_shortest_path_size_when_pathrewind)
-      {
+      if (shortest_path.size() >= previous_shortest_path_size_when_pathrewind) {
         next_vertex_id = previous_vertex_id_;
         backTraceCount_++;
-        if (backTraceCount_ > 15)
-        {
+        if (backTraceCount_ > 15) {
           backTraceCount_ = 0;
           previous_shortest_path_size_when_pathrewind = 100000;
         }
-      }
-      else
-      {
+      } else {
         previous_vertex_id_ = next_vertex_id;
         previous_shortest_path_size_when_pathrewind = shortest_path.size();
       }
-      if (shortest_path.size() <= wrong_id_shortest_path_size_)
-      {
+      if (shortest_path.size() <= wrong_id_shortest_path_size_) {
         wrong_id_ = false;
       }
     }
 
-    geometry_msgs::Point next_waypoint = planned_graph_.vertices[next_vertex_id].location;
-    if (next_vertex_id != shortest_path.back() && (!wrong_id_))
-    {
+    geometry_msgs::Point next_waypoint =
+        planned_graph_.vertices[next_vertex_id].location;
+    if (next_vertex_id != shortest_path.back() && (!wrong_id_)) {
       next_waypoint = projectWayPoint(next_waypoint, robot_pos_);
     }
     waypoint_.header.stamp = ros::Time::now();
@@ -254,45 +224,44 @@ bool GraphPlanner::goToVertex(int current_vertex_idx, int goal_vertex_idx)
 
     // save path for debugging
     planned_path_.clear();
-    for (const auto& v : shortest_path)
-    {
+    for (const auto &v : shortest_path) {
       planned_path_.push_back(planned_graph_.vertices[v].location);
     }
 
-    if (next_vertex_id == shortest_path.back())
-    {
+    if (next_vertex_id == shortest_path.back()) {
       // Assume reached vertex -- publish the last waypoint and then end
       alterAndPublishWaypoint();
       return false;
-    }
-    else
-    {
+    } else {
       // Keep moving along planned path
       return true;
     }
   }
 }
 
-bool GraphPlanner::goToPoint(geometry_msgs::Point point)
-{
-  if (planned_graph_.vertices.size() <= 0)
-  {
+bool GraphPlanner::goToPoint(geometry_msgs::Point point) {
+  if (planned_graph_.vertices.size() <= 0) {
     return false;
   }
   // Find where I am on this graph
-  int current_vertex_idx = graph_utils_ns::GetClosestVertexIdxToPoint(planned_graph_, robot_pos_);
+  int current_vertex_idx =
+      graph_utils_ns::GetClosestVertexIdxToPoint(planned_graph_, robot_pos_);
 
   // find closest goal vertex
-  int goal_vertex_idx = graph_utils_ns::GetClosestVertexIdxToPoint(planned_graph_, point);
+  int goal_vertex_idx =
+      graph_utils_ns::GetClosestVertexIdxToPoint(planned_graph_, point);
 
   // Set the waypoint
   return goToVertex(current_vertex_idx, goal_vertex_idx);
 }
 
-geometry_msgs::Point GraphPlanner::projectWayPoint(geometry_msgs::Point next_vertex_pos, geometry_msgs::Point robot_pos)
-{
-  double ratio = misc_utils_ns::PointXYDist<geometry_msgs::Point, geometry_msgs::Point>(robot_pos, next_vertex_pos) /
-                 kWaypointProjectionDistance;
+geometry_msgs::Point
+GraphPlanner::projectWayPoint(geometry_msgs::Point next_vertex_pos,
+                              geometry_msgs::Point robot_pos) {
+  double ratio =
+      misc_utils_ns::PointXYDist<geometry_msgs::Point, geometry_msgs::Point>(
+          robot_pos, next_vertex_pos) /
+      kWaypointProjectionDistance;
   double x = (next_vertex_pos.x - robot_pos.x) / ratio + robot_pos.x;
   double y = (next_vertex_pos.y - robot_pos.y) / ratio + robot_pos.y;
   double z = (next_vertex_pos.z - robot_pos.z) / ratio + robot_pos.z;
@@ -300,8 +269,7 @@ geometry_msgs::Point GraphPlanner::projectWayPoint(geometry_msgs::Point next_ver
   return way_point;
 }
 
-bool GraphPlanner::initialize()
-{
+bool GraphPlanner::initialize() {
   if (!readParameters())
     return false;
 
@@ -313,13 +281,18 @@ bool GraphPlanner::initialize()
   waypoint_.header.frame_id = world_frame_id_;
 
   // Initialize subscribers
-  graph_sub_ = nh_.subscribe(sub_graph_topic_, 1, &GraphPlanner::graphCallback, this);
-  graph_planner_command_sub_ = nh_.subscribe(graph_planner_command_topic_, 1, &GraphPlanner::commandCallback, this);
-  odometry_sub_ = nh_.subscribe(sub_odometry_topic_, 1, &GraphPlanner::odometryCallback, this);
+  graph_sub_ =
+      nh_.subscribe(sub_graph_topic_, 1, &GraphPlanner::graphCallback, this);
+  graph_planner_command_sub_ = nh_.subscribe(
+      graph_planner_command_topic_, 1, &GraphPlanner::commandCallback, this);
+  odometry_sub_ = nh_.subscribe(sub_odometry_topic_, 1,
+                                &GraphPlanner::odometryCallback, this);
 
   // Initialize publishers
-  waypoint_pub_ = nh_.advertise<geometry_msgs::PointStamped>(pub_waypoint_topic_, 2);
-  graph_planner_status_pub_ = nh_.advertise<graph_planner::GraphPlannerStatus>(graph_planner_status_topic_, 2);
+  waypoint_pub_ =
+      nh_.advertise<geometry_msgs::PointStamped>(pub_waypoint_topic_, 2);
+  graph_planner_status_pub_ = nh_.advertise<graph_planner::GraphPlannerStatus>(
+      graph_planner_status_topic_, 2);
   graph_planner_path_pub_ = nh_.advertise<nav_msgs::Path>(pub_path_topic_, 10);
 
   ROS_INFO("Successfully launched graph_planner node");
@@ -327,8 +300,7 @@ bool GraphPlanner::initialize()
   return true;
 }
 
-void GraphPlanner::executeGoToOrigin()
-{
+void GraphPlanner::executeGoToOrigin() {
   // Aim for (0,0,0)
   geometry_msgs::Point origin;
   origin.x = 0;
@@ -336,76 +308,62 @@ void GraphPlanner::executeGoToOrigin()
   origin.z = 0;
   bool success = goToPoint(origin);
 
-  if (!success)
-  {
+  if (!success) {
     // ROS_INFO("COMMAND_GO_TO_ORIGIN success=false");
     publishInProgress(false);
-  }
-  else
-  {
+  } else {
     // ROS_INFO("COMMAND_GO_TO_ORIGIN success=true");
     alterAndPublishWaypoint();
     publishInProgress(true);
   }
 }
 
-void GraphPlanner::executeGoToLocation()
-{
+void GraphPlanner::executeGoToLocation() {
   // Compute waypoint
   bool success = goToPoint(graph_planner_command_.location);
 
-  if (!success)
-  {
+  if (!success) {
     // ROS_INFO("COMMAND_GO_TO_LOCATION success=false");
     publishInProgress(false);
-  }
-  else
-  {
+  } else {
     // ROS_INFO("COMMAND_GO_TO_LOCATION success=true");
     alterAndPublishWaypoint();
     publishInProgress(true);
   }
 }
 
-void GraphPlanner::executeCommand()
-{
+void GraphPlanner::executeCommand() {
   // Choose from one of the graph_planner variants based on the command
 
-  if (graph_planner_command_.command == graph_planner::GraphPlannerCommand::COMMAND_GO_TO_ORIGIN)
-  {
+  if (graph_planner_command_.command ==
+      graph_planner::GraphPlannerCommand::COMMAND_GO_TO_ORIGIN) {
     // COMMAND_GO_TO_ORIGIN
     // ROS_INFO("graph_planner: COMMAND_GO_TO_ORIGIN");
     executeGoToOrigin();
-  }
-  else if (graph_planner_command_.command == graph_planner::GraphPlannerCommand::COMMAND_GO_TO_LOCATION)
-  {
+  } else if (graph_planner_command_.command ==
+             graph_planner::GraphPlannerCommand::COMMAND_GO_TO_LOCATION) {
     // COMMAND_GO_TO_LOCATION
     // ROS_INFO("graph_planner: COMMAND_GO_TO_LOCATION");
     executeGoToLocation();
-  }
-  else
-  {
+  } else {
     // ROS_WARN("unknown command received");
     publishInProgress(false);
   }
 }
 
-bool GraphPlanner::execute()
-{
+bool GraphPlanner::execute() {
   ros::Rate rate(5);
   bool status = ros::ok();
-  while (status)
-  {
+  while (status) {
     ros::spinOnce();
 
-    if (graph_planner_command_.command == graph_planner::GraphPlannerCommand::COMMAND_DISABLE)
-    {
+    if (graph_planner_command_.command ==
+        graph_planner::GraphPlannerCommand::COMMAND_DISABLE) {
       // Graph planner is disabled -- do nothing
       publishInProgress(false);
-    }
-    else
-    {
-      // Graph planner is enabled -- select waypoint according to current command
+    } else {
+      // Graph planner is enabled -- select waypoint according to current
+      // command
       executeCommand();
     }
 
@@ -419,10 +377,9 @@ bool GraphPlanner::execute()
   return true;
 }
 
-GraphPlanner::GraphPlanner(const ros::NodeHandle& nh)
-{
+GraphPlanner::GraphPlanner(const ros::NodeHandle &nh) {
   nh_ = nh;
   initialize();
 }
 
-}  // namespace graph_planner_ns
+} // namespace graph_planner_ns
